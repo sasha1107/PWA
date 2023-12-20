f2d9ada0ef82a6dddde7913c8a787db810a16087
정수현 <sasha1107@naver.com>
2023-12-20 09:39:37 +0900

    feat: badge test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(navigator.connection)
+  console.log(navigator.connection);
+
+  useEffect(() => {
+    if ('setAppBadge' in navigator) {
+      navigator.setAppBadge(10);
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>

7d94f3c4d855f967bf9a5422cad04a2757e8adbd
정수현 <sasha1107@naver.com>
2023-12-20 13:11:50 +0900

    feat: 내 위치 표시

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { setBadge, sendPush } from './utils';
 import Provider from './components/provider';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 function App() {
   // console.log(navigator);
   const [count, setCount] = React.useState(0);
+  const [state, setState] = useState({
+    center: {
+      lat: 33.450701,
+      lng: 126.570667,
+    },
+    errMsg: null,
+    isLoading: true,
+  });
+
   useEffect(() => {
     setBadge(count);
   }, [count]);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setState((prev) => ({
+            ...prev,
+            center: {
+              lat: position.coords.latitude, // 위도
+              lng: position.coords.longitude, // 경도
+            },
+            isLoading: false,
+          }));
+        },
+        (err) => {
+          setState((prev) => ({
+            ...prev,
+            errMsg: err.message,
+            isLoading: false,
+          }));
+        }
+      );
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+      setState((prev) => ({
+        ...prev,
+        errMsg: 'geolocation을 사용할수 없어요..',
+        isLoading: false,
+      }));
+    }
+  }, []);
+
   console.log(process.env.PUBLIC_URL);
   return (
     <div className='App'>
@@ -58,18 +101,22 @@ function App() {
           내 위치 표시
         </button>
         <Map // 지도를 표시할 Container
-          center={{
-            // 지도의 중심좌표
-            lat: 33.450701,
-            lng: 126.570667,
-          }}
+          center={state.center}
           style={{
             // 지도의 크기
-            width: '100dvw',
-            height: '100dvh',
+            width: '100%',
+            height: '450px',
           }}
           level={3} // 지도의 확대 레벨
-        />
+        >
+          {!state.isLoading && (
+            <MapMarker position={state.center}>
+              <div style={{ padding: '5px', color: '#000' }}>
+                {state.errMsg ? state.errMsg : '여기에 계신가요?!'}
+              </div>
+            </MapMarker>
+          )}
+        </Map>
       </Provider>
     </div>
   );

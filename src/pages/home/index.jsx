db7be7edadecfeee85f16facaa67e2d687db7127
정수현 <sasha1107@naver.com>
2024-01-23 17:43:18 +0900

    feat: Skeleton UI 구현

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,28 +4,42 @@ import { NavLink } from 'react-router-dom';
 const Home = () => {
   const elements = router.routes[0].children;
   return (
-    <nav className="grid h-10 grid-cols-4 divide-x">
-      {elements.map((route, index) => {
-        return (
-          <NavLink
-            key={index}
-            to={route.path}
-            className={({ isActive, isPending, isTransitioning }) =>
-              [
-                isPending ? 'text-slate-300' : '',
-                isActive ? 'text-blue-500' : '',
-                isTransitioning ? 'text-red-500' : '',
-                'flex items-center justify-center'
-              ].join(' ')
-            }
-            unstable_viewTransitions
-          >
-            {route.name}
-          </NavLink>
-        );
-      })}
-    </nav>
+    <>
+      <nav className="grid h-10 grid-cols-4 divide-x">
+        {elements.map((route, index) => {
+          return (
+            <NavLink
+              key={index}
+              to={route.path}
+              className={({ isActive, isPending, isTransitioning }) =>
+                [
+                  isPending ? 'text-slate-300' : '',
+                  isActive ? 'text-blue-500' : '',
+                  isTransitioning ? 'text-red-500' : '',
+                  'flex items-center justify-center'
+                ].join(' ')
+              }
+              unstable_viewTransitions
+            >
+              {route.name}
+            </NavLink>
+          );
+        })}
+      </nav>
+      <Skeleton width={100} height={30} />
+    </>
   );
 };
 
+function Skeleton({ width, height }) {
+  return (
+    <div
+      className="skeleton active"
+      style={{
+        width,
+        height
+      }}
+    ></div>
+  );
+}
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        skeleton: 'active 1s ease-in-out infinite'
+      },
+      keyframes: {
+        active: {
+          '0%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0 50%' }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };

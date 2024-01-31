eb48b3b991a6c1c734798220947de46c49d36792
Soohyun Jung <sasha1107@naver.com>
2024-01-31 21:43:44 +0900

    feat: 연락처 표시

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -3,7 +3,7 @@ import { Support } from 'components';
 
 const Contact = () => {
   const [support, setSupport] = useState(false);
-
+  const [contacts, setContacts] = useState([]);
   useEffect(() => {
     if ('contacts' in navigator && 'ContactsManager' in window) {
       setSupport(true);
@@ -11,21 +11,32 @@ const Contact = () => {
   }, []);
   const onClickBtn = async () => {
     if ('contacts' in navigator && 'ContactsManager' in window) {
-      const contacts = await navigator.contacts.select(['name', 'email'], {
-        multiple: true
-      });
-      // const contacts = await navigator.contacts.select(props, {
-      //   multiple: true
-      // });
-      console.log(contacts);
+      const contacts = await navigator.contacts.select(
+        ['name', 'email', 'tel'],
+        {
+          multiple: true
+        }
+      );
+      setContacts(contacts);
     }
-
-    // showContacts(contacts);
   };
+
   return (
     <div>
       <Support support={support} />
+
       <button onClick={onClickBtn}>연락처</button>
+      <div>
+        {contacts.map((contact) => (
+          <div className="card flex justify-between" key={contact.name}>
+            <div>{contact.name}</div>
+            <div>
+              <a href={`tel:+82${contact.tel}`}>{contact.tel}</a>
+              <a href={`mailto:${contact.email}`}>{contact.email}</a>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

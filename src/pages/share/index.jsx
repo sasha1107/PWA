b5b4af9b5c1729e9a2e12ebff1d99e52fe2f0ebd
정수현 <sasha1107@naver.com>
2024-02-01 15:53:38 +0900

    feat: 공유 데이터 추가

--- src/pages/share/index.jsx
+++ src/pages/share/index.jsx
@@ -3,27 +3,61 @@ import { Support } from 'components';
 
 const Share = () => {
   const [support, setSupport] = useState(false);
-  const shareData = {
-    title: 'MDN',
-    text: 'Learn web development on MDN!',
-    url: 'https://developer.mozilla.org'
-  };
+  const [data, setData] = useState({
+    title: '',
+    text: '',
+    url: ''
+  });
 
   useEffect(() => {
     if (navigator.canShare) {
       setSupport(true);
     }
   }, []);
+
   return (
-    <div>
+    <div className="flex flex-col">
       <Support support={support} />
+
+      <h1>공유</h1>
+      <div className="border p-2">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="title">title</label>
+          <input
+            id="title"
+            type="text"
+            value={data.title}
+            onChange={(e) => setData({ ...data, title: e.target.value })}
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="text">text</label>
+          <input
+            id="text"
+            type="text"
+            value={data.text}
+            onChange={(e) => setData({ ...data, text: e.target.value })}
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="url">url</label>
+          <input
+            id="url"
+            type="text"
+            value={data.url}
+            onChange={(e) => setData({ ...data, url: e.target.value })}
+          />
+        </div>
+      </div>
+
       <button
+        className="primary"
         onClick={async () => {
           try {
             if (!navigator.canShare) {
               return;
             }
-            await navigator.share(shareData);
+            await navigator.share(data);
           } catch (err) {
             console.log(err);
           }
